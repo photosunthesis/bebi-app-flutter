c93e1b2fe929cfe53025978be35ab990e40d225e
Sun Envidiado <sun.adriann@icloud.com>
2025-08-14 17:08:03 +0800

    refactor: ♻️ Move app dependencies module inside the dependencies config

--- lib/config/dependencies_config.dart
+++ lib/config/dependencies_config.dart
@@ -1,7 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 
 import 'dependencies_config.config.dart';
 
 @InjectableInit()
 Future<void> configureDependencies() async => GetIt.I.init();
+
+@module
+abstract class OtherDependencies {
+  ImagePicker get imagePicker => ImagePicker();
+}
