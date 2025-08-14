eafa851bf8304dd40201ac1a4af5a12b9f418ef1
Sun Envidiado <sun.adriann@icloud.com>
2025-08-14 16:59:53 +0800

    refactor: 🚚 Rename dependency injection config

--- lib/config/dependencies_config.dart
+++ lib/config/dependencies_config.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
-import 'injectable.config.dart';
+import 'dependencies_config.config.dart';
 
 @InjectableInit()
 Future<void> configureDependencies() async => GetIt.I.init();
