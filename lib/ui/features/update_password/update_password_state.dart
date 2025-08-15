6a7269d267278fc0413477f71c1220a2be67d147
Sun Envidiado <sun.adriann@icloud.com>
2025-08-15 16:09:30 +0800

    fix: 🚨 Fix small lint warning

--- lib/ui/features/update_password/update_password_state.dart
+++ lib/ui/features/update_password/update_password_state.dart
@@ -5,5 +5,6 @@ sealed class UpdatePasswordState with _$UpdatePasswordState {
   const factory UpdatePasswordState.data() = UpdatePasswordStateData;
   const factory UpdatePasswordState.loading() = UpdatePasswordStateLoading;
   const factory UpdatePasswordState.success() = UpdatePasswordStateSuccess;
-  const factory UpdatePasswordState.error(String error) = UpdatePasswordStateError;
+  const factory UpdatePasswordState.error(String error) =
+      UpdatePasswordStateError;
 }
