752db6f1ebf95006fb5b51ad80f2f00ccb611c8e
Sun Envidiado <sun.adriann@icloud.com>
2025-09-01 13:27:33 +0800

    fix: 🐛 update keystore file paths in build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
+val keystorePropertiesFile = project.rootProject.file("../key.properties")
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
