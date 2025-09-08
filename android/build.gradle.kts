50a676a6d04a3bf4a11e314a966135d2144369a7
Sun Envidiado <sun.adriann@icloud.com>
2025-09-08 19:06:25 +0800

    chore: ⬆️ upgrade to Flutter v3.35.3 dependencies and configurations
    - Update Flutter and Dart SDK constraints in pubspec.yaml
    - Bump Firebase dependencies in Podfile.lock to latest versions
    - Update Gradle and Kotlin versions in build.gradle.kts and settings.gradle.kts
    - Add missing platform revisions in .metadata for Android and iOS
    - Update Android build configurations to use Java 17
    - Add build directory to .gitignore for Android
    - Update Gradle wrapper to version 8.13
    - Update various packages in pubspec.lock to their latest versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,17 +1,29 @@
-allprojects {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
+    val kotlinVersion by extra("2.1.0")
+    val agpVersion by extra("8.12.0")
+    
+    dependencies {
+        classpath("com.android.tools.build:gradle:$agpVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
