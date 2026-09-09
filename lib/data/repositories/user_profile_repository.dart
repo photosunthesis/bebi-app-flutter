a6624df229d1a3f36d893eb117f1a859cc6b6b53
Sun Envidiado <sune@mercola.com>
2026-09-09 17:12:55 +0800

    chore: 🧹 clean up imports by adding missing newlines in multiple files

--- lib/data/repositories/user_profile_repository.dart
+++ lib/data/repositories/user_profile_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:bebi_app/data/models/user_profile.dart';
 import 'package:bebi_app/data/services/image_storage_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
--- lib/ui/shared_widgets/specialized/angled_stripes_background.dart
+++ lib/ui/shared_widgets/specialized/angled_stripes_background.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+
 import 'package:material_ui/material_ui.dart';
 
 class AngledStripesBackground extends StatelessWidget {
